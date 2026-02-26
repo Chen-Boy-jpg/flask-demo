955dddcb80c4a09ff7d0574a9ff4c587008177bd
unknown <yyy71243@gmail.com>
2026-02-26 16:57:15 +0800

    04-jinja2

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/")
 def home():
-    return render_template("index.html", name="Daniel")
+    posts = [
+        {"title": "第一篇文章", "content": "這是第一篇文章內容"},
+        {"title": "第二篇文章", "content": "這是第二篇文章內容"},
+        {"title": "第三篇文章", "content": "這是第三篇文章內容"},
+    ]
+    show_ads = True
+    return render_template("index.html", posts=posts, show_ads=show_ads)
 
 if __name__ == "__main__":
     app.run(debug=True)
