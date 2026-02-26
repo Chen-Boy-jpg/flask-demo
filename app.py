0dda7f8e018cc2de77dd471276f01fef03e3ac70
unknown <yyy71243@gmail.com>
2026-02-26 16:50:30 +0800

    02-flask-route

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Flask!"
+    return "這是首頁"
+
+@app.route("/about")
+def about():
+    return "這是關於頁面"
+
+@app.route("/hello/<name>")
+def hello(name):
+    return f"Hello {name}"
 
 if __name__ == "__main__":
     app.run(debug=True)
